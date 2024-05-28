4165307d48a4788badc6abf1e42c490a0ec5d813
Samuel Finder <Samuel.Finder@bbd.co.za>
2024-05-28 21:01:14 +0200

    updated region

--- DB/infrastructure/main.tf
+++ DB/infrastructure/main.tf
@@ -39,7 +39,7 @@ resource "aws_subnet" "crm_subnet_a" {
   vpc_id                  = aws_vpc.crm_vpc.id
   cidr_block              = "10.0.4.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = var.AWS_REGION
+  availability_zone       = "eu-west-1a"
   tags = {
     owner: "samuel.finder@bbd.co.za"
   }
