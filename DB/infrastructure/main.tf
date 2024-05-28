3abbedf7de554b2c79d45b4d34d491cc84a11d23
Samuel Finder <Samuel.Finder@bbd.co.za>
2024-05-28 20:54:55 +0200

    changed lower casing

--- DB/infrastructure/main.tf
+++ DB/infrastructure/main.tf
@@ -19,24 +19,24 @@ provider "aws" {
   region = var.AWS_REGION
 }
 
-resource "aws_vpc" "CRM_vpc" {
+resource "aws_vpc" "crm_vpc" {
   cidr_block = "10.0.0.0/16"
   enable_dns_support = true
   enable_dns_hostnames = true
   tags = {
-    Name = "CRM_vpc"
+    Name = "crm_vpc"
   }
 }
 
-resource "aws_internet_gateway" "CRM_gateway" {
-  vpc_id = aws_vpc.CRM_vpc.id
+resource "aws_internet_gateway" "crm_gateway" {
+  vpc_id = aws_vpc.crm_vpc.id
   tags = {
     owner = "samuel.finder@bbd.co.za"
   }
 }
 
-resource "aws_subnet" "CRM_subnet_a" {
-  vpc_id                  = aws_vpc.CRM_vpc.id
+resource "aws_subnet" "crm_subnet_a" {
+  vpc_id                  = aws_vpc.crm_vpc.id
   cidr_block              = "10.0.4.0/24"
   map_public_ip_on_launch = true
   availability_zone       = var.AWS_REGION
@@ -45,12 +45,12 @@ resource "aws_subnet" "CRM_subnet_a" {
   }
 }
 
-resource "aws_route_table" "CRM_route_table_a" {
-  vpc_id = aws_vpc.CRM_vpc.id
+resource "aws_route_table" "crm_route_table_a" {
+  vpc_id = aws_vpc.crm_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.CRM_gateway.id
+    gateway_id = aws_internet_gateway.crm_gateway.id
   }
 
   tags = {
@@ -58,13 +58,13 @@ resource "aws_route_table" "CRM_route_table_a" {
   }
 }
 
-resource "aws_route_table_association" "CRM_association_a" {
-  subnet_id      = aws_subnet.CRM_subnet_a.id
-  route_table_id = aws_route_table.CRM_route_table_a.id
+resource "aws_route_table_association" "crm_association_a" {
+  subnet_id      = aws_subnet.crm_subnet_a.id
+  route_table_id = aws_route_table.crm_route_table_a.id
 }
 
-resource "aws_subnet" "CRM_subnet_b" {
-  vpc_id                  = aws_vpc.CRM_vpc.id
+resource "aws_subnet" "crm_subnet_b" {
+  vpc_id                  = aws_vpc.crm_vpc.id
   cidr_block              = "10.0.5.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "eu-west-1b"
@@ -73,13 +73,13 @@ resource "aws_subnet" "CRM_subnet_b" {
   }
 }
 
-resource "aws_route_table" "CRM_route_table_b" {
-    vpc_id = aws_vpc.CRM_vpc.id
+resource "aws_route_table" "crm_route_table_b" {
+    vpc_id = aws_vpc.crm_vpc.id
 
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.CRM_gateway.id
+    gateway_id = aws_internet_gateway.crm_gateway.id
   }
 
   tags = {
@@ -87,22 +87,22 @@ resource "aws_route_table" "CRM_route_table_b" {
   }
 }
 
-resource "aws_route_table_association" "CRM_association_b" {
-  subnet_id      = aws_subnet.CRM_subnet_b.id
-  route_table_id = aws_route_table.CRM_route_table_b.id
+resource "aws_route_table_association" "crm_association_b" {
+  subnet_id      = aws_subnet.crm_subnet_b.id
+  route_table_id = aws_route_table.crm_route_table_b.id
 }
 
-resource "aws_db_subnet_group" "CRM_subnet_group" {
-  name       = "CRM_subnet_group"
-  subnet_ids = [aws_subnet.CRM_subnet_a.id, aws_subnet.CRM_subnet_b.id]
+resource "aws_db_subnet_group" "crm_subnet_group" {
+  name       = "crm_subnet_group"
+  subnet_ids = [aws_subnet.crm_subnet_a.id, aws_subnet.crm_subnet_b.id]
 
   tags = {
     owner: "samuel.finder@bbd.co.za"
   }
 }
 
-resource "aws_security_group" "CRM_security_group" {
-  vpc_id = aws_vpc.CRM_vpc.id
+resource "aws_security_group" "crm_security_group" {
+  vpc_id = aws_vpc.crm_vpc.id
 
   ingress {
     from_port   = 5432
@@ -123,20 +123,20 @@ resource "aws_security_group" "CRM_security_group" {
   }
 }
 
-resource "aws_db_instance" "CRM_postgres_rds" {
+resource "aws_db_instance" "crm_postgres_rds" {
   allocated_storage    = 5
   instance_class       = "db.t3.micro"
   publicly_accessible  = true
   skip_final_snapshot  = true
   multi_az             = false
   engine               = "postgres"
-  identifier           = "CRM-postgres-db"
-  db_name              = "CRM_DB"
+  identifier           = "crm-postgres-db"
+  db_name              = "crm_DB"
   username             = var.DB_USERNAME
   password             = var.DB_PASSWORD
 
-  vpc_security_group_ids = [aws_security_group.CRM_security_group.id]
-  db_subnet_group_name = aws_db_subnet_group.CRM_subnet_group.name
+  vpc_security_group_ids = [aws_security_group.crm_security_group.id]
+  db_subnet_group_name = aws_db_subnet_group.crm_subnet_group.name
 
 
   tags = {
