9fe4339f7bfd54c6261b1173654d1544f7b93569
Verushan Naidoo <verushan.naidoo@bbd.co.za>
2024-06-05 01:40:52 +0200

    Implemented OAuth on frontend

---
+++ crm-tool/src/aws-exports.js
@@ -0,0 +1,39 @@
+const awsmobile = {
+    "aws_project_region": import.meta.env.VITE_AWS_REGION,
+    "aws_cognito_identity_pool_id": import.meta.env.VITE_USER_POOL_ID,
+    "aws_cognito_region": import.meta.env.VITE_AWS_REGION,
+    "aws_user_pools_id": import.meta.env.VITE_USER_POOL_ID,
+    "aws_user_pools_web_client_id": import.meta.env.VITE_CLIENT_ID,
+    "oauth": {
+        "domain": import.meta.env.VITE_DOMAIN,
+        "scope": [
+            "phone",
+            "email",
+            "openid",
+        ],
+        "redirectSignIn": import.meta.env.VITE_REDIRECT_URI,
+        "responseType": "code"
+    },
+    "federationTarget": "COGNITO_USER_POOLS",
+    "aws_cognito_username_attributes": [
+        "EMAIL"
+    ],
+    "aws_cognito_social_providers": [],
+    "aws_cognito_signup_attributes": [
+        "EMAIL"
+    ],
+    "aws_cognito_mfa_configuration": "OFF",
+    "aws_cognito_mfa_types": [
+        "SMS"
+    ],
+    "aws_cognito_password_protection_settings": {
+        "passwordPolicyMinLength": 8,
+        "passwordPolicyCharacters": []
+    },
+    "aws_cognito_verification_mechanisms": [
+        "EMAIL"
+    ]
+};
+
+
+export default awsmobile;
