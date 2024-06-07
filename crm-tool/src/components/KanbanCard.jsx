bd81a37d105e46a1bd4de64300036cdd573b642f
Samuel Finder <Samuel.Finder@bbd.co.za>
2024-06-07 09:26:55 +0200

    fixed roles

--- crm-tool/src/components/KanbanCard.jsx
+++ crm-tool/src/components/KanbanCard.jsx
@@ -7,86 +7,92 @@ import { fetchAuthSession } from "aws-amplify/auth";
 import "./KanbanCard.css";
 
 const KanbanCard = ({ cardDetails, index }) => {
-  const navigate = useNavigate();
-  const [userRole, setUserRole] = useState(null);
+	const navigate = useNavigate();
+	const [userRole, setUserRole] = useState(null);
 
-  useEffect(() => {
-    const fetchUserRole = async () => {
-      try {
-        const idToken = (await fetchAuthSession()).tokens?.idToken?.toString();
+	useEffect(() => {
+		const fetchUserRole = async () => {
+			try {
+				const idToken = (await fetchAuthSession()).tokens?.idToken?.toString();
 
-        if (!idToken) {
-          throw new Error("ID token not found.");
-        }
+				if (!idToken) {
+					throw new Error("ID token not found.");
+				}
 
-        const tokenParts = idToken.split(".");
-        const payload = JSON.parse(atob(tokenParts[1]));
-        const fetchedUserRole = payload["cognito:groups"][1];
-        setUserRole(fetchedUserRole);
-      } catch (error) {
-        console.error("Error retrieving user role:", error);
-      }
-    };
+				const tokenParts = idToken.split(".");
+				const payload = JSON.parse(atob(tokenParts[1]));
+				console.log(payload["cognito:groups"]);
 
-    fetchUserRole();
-  }, []);
+				if (payload["cognito:groups"].includes("Manager")) {
+					setUserRole("Manager");
+				} else if (payload["cognito:groups"].includes("SalesRep")) {
+					setUserRole("SalesRep");
+				} else {
+					setUserRole("GeneralUser");
+				}
+			} catch (error) {
+				console.error("Error retrieving user role:", error);
+			}
+		};
 
-  const openPopUp = (cardDetails) => {
-    if (userRole === "SalesRep" || userRole === "Manager") {
-      navigate(`/viewOpp`, { state: { cardDetails } });
-    }
-  };
+		fetchUserRole();
+	}, []);
+	const openPopUp = (cardDetails) => {
+		if (userRole === "SalesRep" || userRole === "Manager") {
+			navigate(`/viewOpp`, { state: { cardDetails } });
+		}
+	};
 
-  const getBoxShadow = (stage) => {
-    switch (stage) {
-      case 1:
-        return "custom-orange";
-      case 2:
-        return "custom-maroon";
-      case 3:
-        return "custom-red";
-      case 4:
-        return "custom-purple";
-      case 5:
-        return "custom-navy";
-      default:
-        return "";
-    }
-  };
+	const getBoxShadow = (stage) => {
+		switch (stage) {
+			case 1:
+				return "custom-orange";
+			case 2:
+				return "custom-maroon";
+			case 3:
+				return "custom-red";
+			case 4:
+				return "custom-purple";
+			case 5:
+				return "custom-navy";
+			default:
+				return "";
+		}
+	};
 
-  return (
-    <Draggable
-      draggableId={cardDetails.opportunityID.toString()}
-      index={index}
-      gap="2vh"
-    >
-      {(provided) => (
-        <Flex
-          ref={provided.innerRef}
-          {...provided.draggableProps}
-          {...provided.dragHandleProps}
-          bg="white"
-          height="10vh"
-          width="100%"
-          flexDir="column"
-          justifyContent="center"
-          alignItems="center"
-          cursor="pointer"
-          padding="1vh"
-          gap="1vh"
-          boxShadow={getBoxShadow(cardDetails.stage)}
-          onClick={() => openPopUp(cardDetails)}
-        >
-          <Box>
-            <h4 className="title">{cardDetails.title}</h4>
-          </Box>
-          <Box>
-            <p className="text">{cardDetails.amount}</p>
-          </Box>
-        </Flex>
-      )}
-    </Draggable>
-  );
+	return (
+		<Draggable
+			draggableId={cardDetails.opportunityID.toString()}
+			index={index}
+			gap="2vh"
+		>
+			{(provided) => (
+				<Flex
+					ref={provided.innerRef}
+					{...provided.draggableProps}
+					{...provided.dragHandleProps}
+					bg="white"
+					height="10vh"
+					width="100%"
+					flexDir="column"
+					justifyContent="center"
+					alignItems="center"
+					cursor="pointer"
+					padding="1vh"
+					gap="1vh"
+					boxShadow={getBoxShadow(cardDetails.stage)}
+					onClick={() => openPopUp(cardDetails)}
+				>
+					<Box>
+						<h4 className="title">{cardDetails.title}</h4>
+					</Box>
+					<Box>
+						<p className="text">{cardDetails.amount}</p>
+					</Box>
+				</Flex>
+			)}
+		</Draggable>
+	);
 };
 
 export default KanbanCard;
--- crm-tool/src/pages/Pipeline.jsx
+++ crm-tool/src/pages/Pipeline.jsx
@@ -9,122 +9,111 @@ import add_circle from "../assets/add_circle.svg";
 import { fetchAuthSession } from "aws-amplify/auth";
 
 const Pipeline = () => {
-  const navigate = useNavigate();
-  const { colorMode } = useColorMode();
-  const [userRole, setUserRole] = useState(null);
-  const [opportunities, setOpportunities] = useState([]);
-  const [loading, setLoading] = useState(true);
+	const navigate = useNavigate();
+	const { colorMode } = useColorMode();
+	const [userRole, setUserRole] = useState(null);
+	const [opportunities, setOpportunities] = useState([]);
+	const [loading, setLoading] = useState(true);
+	useEffect(() => {
+		const fetchUserRole = async () => {
+			try {
+				const idToken = (await fetchAuthSession()).tokens?.idToken?.toString();
 
-  useEffect(() => {
-    const postLogin = async () => {
-      try {
-        await opportunityService.postLoginInfo();
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
+				if (!idToken) {
+					throw new Error("ID token not found.");
+				}
 
-    postLogin();
-  }, []); // Empty dependency array ensures this runs once on component mount
+				const tokenParts = idToken.split(".");
+				const payload = JSON.parse(atob(tokenParts[1]));
+				console.log(payload["cognito:groups"]);
 
-  useEffect(() => {
-    const fetchUserRole = async () => {
-      try {
-        const idToken = (await fetchAuthSession()).tokens?.idToken?.toString();
+				if (payload["cognito:groups"].includes("Manager")) {
+					setUserRole("Manager");
+				} else if (payload["cognito:groups"].includes("SalesRep")) {
+					setUserRole("SalesRep");
+				} else {
+					setUserRole("GeneralUser");
+				}
+			} catch (error) {
+				console.error("Error retrieving user role:", error);
+			}
+		};
 
-        if (!idToken) {
-          throw new Error("ID token not found.");
-        }
+		fetchUserRole();
+	}, []);
 
-        const tokenParts = idToken.split(".");
-        const payload = JSON.parse(atob(tokenParts[1]));
-        const fetchedUserRole = payload["cognito:groups"][1];
-        setUserRole(fetchedUserRole);
-      } catch (error) {
-        console.error("Error retrieving user role:", error);
-      }
-    };
+	useEffect(() => {
+		const fetchSalesOpportunities = async () => {
+			try {
+				const opportunities =
+					await opportunityService.fetchSalesOpportunities();
+				setOpportunities(opportunities);
+			} catch (error) {
+				console.error("Error fetching sales opportunities:", error);
+			} finally {
+				setLoading(false);
+			}
+		};
 
-    fetchUserRole();
-  }, []);
+		const fetchSalesOpportunitiesCurrentUser = async () => {
+			try {
+				const opportunities = await opportunityService.fetchOpportunityById();
+				setOpportunities(opportunities);
+			} catch (error) {
+				console.error(
+					"Error fetching current user's sales opportunities:",
+					error
+				);
+			} finally {
+				setLoading(false);
+			}
+		};
 
-  useEffect(() => {
-    const fetchSalesOpportunities = async () => {
-      try {
-        const opportunities =
-          await opportunityService.fetchSalesOpportunities();
-        setOpportunities(opportunities);
-      } catch (error) {
-        console.error("Error fetching sales opportunities:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
+		if (userRole) {
+			if (userRole === "Manager" || userRole === "GeneralUser") {
+				fetchSalesOpportunities();
+			} else if (userRole === "SalesRep") {
+				fetchSalesOpportunitiesCurrentUser();
+			}
+		}
+	}, [userRole]);
 
-    const fetchSalesOpportunitiesCurrentUser = async () => {
-      try {
-        const opportunities = await opportunityService.fetchOpportunityById();
-        setOpportunities(opportunities);
-      } catch (error) {
-        console.error(
-          "Error fetching current user's sales opportunities:",
-          error
-        );
-      } finally {
-        setLoading(false);
-      }
-    };
+	const handleNew = () => {
+		navigate(`/NewOpp`);
+	};
 
-    if (userRole) {
-      if (userRole === "Manager" || userRole === "GeneralUser") {
-        console.log("user is manager or general user");
-        fetchSalesOpportunities();
-      } else if (userRole === "SalesRep") {
-        console.log("user is salesRep");
-        // RETURNING 404
-        fetchSalesOpportunitiesCurrentUser();
-      }
-    }
-  }, [userRole]);
-
-  const handleNew = () => {
-    navigate(`/NewOpp`);
-  };
-
-  return (
-    <Flex
-      bg={colorMode === "light" ? "brand.light_grey" : "brand.navy"}
-      className="page-container"
-    >
-      <SideNav />
-      <Flex className="main-container">
-        <Flex
-          width="100%"
-          color="black"
-          height="5vh"
-          justify="flex-end"
-          paddingRight="4vh"
-        >
-          <Button
-            className="button"
-            bg="brand.orange"
-            color="brand.white"
-            onClick={handleNew}
-          >
-            <img src={add_circle} alt="add opportunity icon" />
-            Add New
-          </Button>
-        </Flex>
-        {loading ? (
-          <Skeleton width="100px" height="20px" />
-        ) : (
-          <KanbanBoard data={opportunities} />
-        )}
-      </Flex>
-    </Flex>
-  );
+	return (
+		<Flex
+			bg={colorMode === "light" ? "brand.light_grey" : "brand.navy"}
+			className="page-container"
+		>
+			<SideNav />
+			<Flex className="main-container">
+				<Flex
+					width="100%"
+					color="black"
+					height="5vh"
+					justify="flex-end"
+					paddingRight="4vh"
+				>
+					<Button
+						className="button"
+						bg="brand.orange"
+						color="brand.white"
+						onClick={handleNew}
+					>
+						<img src={add_circle} alt="add opportunity icon" />
+						Add New
+					</Button>
+				</Flex>
+				{loading ? (
+					<Skeleton width="100px" height="20px" />
+				) : (
+					<KanbanBoard data={opportunities} />
+				)}
+			</Flex>
+		</Flex>
+	);
 };
 
 export default Pipeline;
